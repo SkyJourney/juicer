4fee02d42a1013443016e904c8a66bb7d5497f0f
SkyJourney <sky.jiang.s.z@gmail.com>
2020-01-07 08:53:31 +0800

    Add JavaDoc

--- src/main/java/com/juicer/JuicerCollector.java
+++ src/main/java/com/juicer/JuicerCollector.java
@@ -4,11 +4,14 @@ import com.juicer.core.Headers;
 import com.juicer.core.JuicerData;
 import com.juicer.core.JuicerHandler;
 import com.juicer.util.DocumentHelper;
+import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -31,70 +34,46 @@ public class JuicerCollector {
 
     public List<JuicerData> getDataFromHandler(String handlerBean){
         Headers headers = DocumentHelper.getSampleHeaders();
-        return getDataFromHandler(null, handlerBean, headers, null);
-    }
-
-    public List<JuicerData> getDataFromHandler(JuicerData transferData, String handlerBean){
-        Headers headers = DocumentHelper.getSampleHeaders();
-        return getDataFromHandler(transferData, handlerBean, headers, null);
+        return getDataFromHandler(handlerBean, headers, null);
     }
 
     public List<JuicerData> getDataFromHandler(String handlerBean, Headers headers){
-        return getDataFromHandler(null, handlerBean, headers, null);
-    }
-
-    public List<JuicerData> getDataFromHandler(JuicerData transferData, String handlerBean, Headers headers){
-        return getDataFromHandler(transferData, handlerBean, headers, null);
+        return getDataFromHandler(handlerBean, headers, null);
     }
 
     public List<JuicerData> getDataFromHandler(String handlerBean, JuicerData juicerData){
         Headers headers = DocumentHelper.getSampleHeaders();
-        return getDataFromHandler(null, handlerBean, headers, juicerData);
+        return getDataFromHandler(handlerBean, headers, juicerData);
     }
 
-    public List<JuicerData> getDataFromHandler(JuicerData transferData, String handlerBean, JuicerData juicerData){
-        Headers headers = DocumentHelper.getSampleHeaders();
-        return getDataFromHandler(transferData, handlerBean, headers, juicerData);
-    }
-
-    public List<JuicerData> getDataFromHandler(JuicerData transferData, String handlerBean, Headers headers,JuicerData juicerData){
+    public List<JuicerData> getDataFromHandler(String handlerBean, Headers headers,JuicerData juicerData){
         JuicerHandler juicerHandler = juicerHandlerFactory.getJuicerHandler(handlerBean);
-        Stream<URL> stream;
-        Function<JuicerData, Stream<URL>> getUrlsFromParent = preData -> {
+        Stream<Map.Entry<JuicerData,URL>> stream;
+        Function<JuicerData, Stream<Map.Entry<JuicerData,URL>>> getUrlsFromParent = preData -> {
             if(preData!=null && preData.get("_source")!=null){
                 headers.put("referer", (String)preData.get("_source"));
             }
-            return juicerHandler.getUrls(preData).stream();
+            return juicerHandler.getUrls(preData).stream().map(url -> new AbstractMap.SimpleEntry<>(preData, url));
         };
-
-        if(transferData != null){
-            if(juicerHandler.hasParent() && juicerData==null){
-                stream = this.getDataFromHandler(transferData,juicerHandler.getParent(),headers).parallelStream()
-                        .flatMap(getUrlsFromParent);
-            } else {
-                stream = juicerHandler.getUrls(juicerData).parallelStream();
-            }
+        if(juicerHandler.hasParent() && juicerData==null){
+            stream = this.getDataFromHandler(juicerHandler.getParent(),headers).parallelStream()
+                    .flatMap(getUrlsFromParent);
         } else {
-            if(juicerHandler.hasParent() && juicerData==null){
-                stream = this.getDataFromHandler(juicerHandler.getParent(),headers).parallelStream()
-                        .flatMap(getUrlsFromParent);
-            } else {
-                stream = juicerHandler.getUrls(juicerData).parallelStream();
-            }
+            stream = juicerHandler.getUrls(juicerData).parallelStream().map(url -> new AbstractMap.SimpleEntry<>(juicerData, url));
         }
         if(juicerData!=null && juicerData.get("_source")!=null){
             headers.put("referer", (String)juicerData.get("_source"));
         }
-        return stream.map(URL::toExternalForm)
-                .map(url -> {
+        return stream
+                .map(entry -> {
+                    Connection.Response response;
                     try {
-                        return DocumentHelper.getResponse(url, headers);
+                        response = DocumentHelper.getResponse(entry.getValue().toExternalForm(), headers);
                     } catch (Exception e) {
                         e.printStackTrace();
                         throw new RuntimeException("Cannot get response.");
                     }
-                }).filter(Objects::nonNull)
-                .map(response -> {
+                    Objects.requireNonNull(response);
                     Document document;
                     try {
                         document = response.parse();
@@ -104,8 +83,11 @@ public class JuicerCollector {
                     }
                     Objects.requireNonNull(document);
                     String html = document.html();
-                    return juicerHandler.parse(transferData, response, document, html);
+                    if(entry.getKey()!=null){
+                        return juicerHandler.parse(JuicerData.getInstance().addData(entry.getKey()), response, document, html);
+                    } else {
+                        return juicerHandler.parse(JuicerData.getInstance(), response, document, html);
+                    }
                 }).collect(Collectors.toList());
     }
-
 }
